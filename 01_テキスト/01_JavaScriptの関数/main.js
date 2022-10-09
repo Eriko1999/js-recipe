0448da7c196ae6acc514136d52b6b9e8edaf4fa3
Eriko1999 <erikokurata7@gmail.com>
2022-10-09 09:04:55 +0200

    [カウンター]を学習しました。

--- 01_テキスト/01_JavaScriptの関数/main.js
+++ 01_テキスト/01_JavaScriptの関数/main.js
@@ -28,6 +28,6 @@ const multiply = function(x, y){
   return x * y
 }
 // ② ２つの引数を受け取り、それらを足し合わせた結果を四捨五入した値を返す関数 plusRound
-const plusRound = (x, y){
+const plusRound = (x, y) {
   return Math.round(x + y)
 }
